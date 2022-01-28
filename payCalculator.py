8ea45d6c2142b5b378e015d3663f475612a1af34
Liz Lass <Liz.Lass@vu.com>
2022-01-28 10:54:11 -0600

    code

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,16 @@
+from http.client import PAYMENT_REQUIRED
+
+
 def calculatePay():
     
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = float(input("Enter Hours:"))
+    rate = float(input("Enter Rate:"))
+    if (hrs > 40.0):
+        OTpay = (rate*.5)*(hrs-40)
+    pay = ((rate*hrs) + OTpay)
+    print ("pay", str(pay))
+        
     
     # end assignment
 
@@ -9,4 +18,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+#calculatePay()
